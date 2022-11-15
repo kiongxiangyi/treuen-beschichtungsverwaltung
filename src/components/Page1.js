ac6bb9b4e163c23d3f8e16f3fd010896b073677e
Xiang Yi Kiong <kiongxiangyi@gmail.com>
2022-11-15 15:59:49 +0100

    page1 links with backend DB

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 
 export default function Page1() {
   const [fertigungsauftrag, setFertigungsauftrag] = useState("");
+  const [fertigungsauftragDB, setFertigungsauftragDB] = useState("");
 
   const navigate = useNavigate(); //hook for navigation
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    navigate("/page2");
+    if (!fertigungsauftrag)
+      //if the scan filed is empty
+      return toast.error("Bitte Fertigungsauftrag scannen!");
+
+    //find auftragsnummer in DB
+    const findAuftrag = fertigungsauftragDB.find(
+      (tblAuftragArtikel) =>
+        tblAuftragArtikel.Auftragsnummer === fertigungsauftrag
+    );
+
+    //if not found, error. If found, navigate to second page
+    if (!findAuftrag) {
+      return toast.error("Die Fertigungsauftragsnummer ist nicht vorhanden");
+    } else {
+      navigate("/page2");
+    }
   };
+
+  //get data from DB
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API}/AuftragArtikel`)
+      .then((res) => res.json())
+      .then((results) => setFertigungsauftragDB(results)) //fetch artikel from tblAuftragArtikel
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
@@ -26,6 +52,7 @@ export default function Page1() {
             onChange={(e) => setFertigungsauftrag(e.target.value)}
           />
         </form>
+        <ToastContainer />
       </div>
       <div className="storage-bin">
         <p>Freie Lagerplätze:</p>
